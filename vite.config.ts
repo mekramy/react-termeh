12b881809d62db4d4c324196493d97b901308604
Mojtaba ajamekramy <mekramy@users.noreply.github.com>
2025-10-25 17:35:48 +0330

    feat: add project setup

---
+++ vite.config.ts
@@ -0,0 +1,38 @@
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+import { viteStaticCopy } from "vite-plugin-static-copy";
+import pkg from "./package.json";
+
+/// <reference types="vitest" />
+// Configure Vitest (https://vitest.dev/config/)
+// https://vitejs.dev/config/
+export default defineConfig({
+    build: {
+        sourcemap: true,
+        lib: {
+            formats: ["es"],
+            name: "ReactTermeh",
+            entry: resolve(__dirname, "src", "index.ts"),
+            fileName: (format) => `index.${format}.js`,
+        },
+        rollupOptions: {
+            external: [...Object.keys(pkg.peerDependencies || {})],
+        },
+    },
+    plugins: [
+        react({
+            babel: {
+                plugins: [["babel-plugin-react-compiler"]],
+            },
+        }),
+        dts({
+            insertTypesEntry: true,
+            copyDtsFiles: true,
+        }),
+        viteStaticCopy({
+            targets: [{ src: "./src/style.scss", dest: "." }],
+        }),
+    ],
+});
